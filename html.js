3fe000b490a1f0542438a81dfd9f75cce7423ad8
Christoph Weber <kriswep@wetainment.com>
2016-12-06 00:19:47 +0100

    pwaify

--- html.js
+++ html.js
@@ -15,25 +15,29 @@ module.exports = React.createClass({
         const font = <link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,500,700&subset=latin,cyrillic' rel='stylesheet' type='text/css' />
         let css
         if (process.env.NODE_ENV === 'production') {
-            css = <style dangerouslySetInnerHTML={ {    __html: require('!raw!./public/styles.css')} } />
+            css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />
         }
 
         return (
             <html lang="en">
-            <head>
-              <meta charSet="utf-8" />
-              <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
-              <title>
-                { title }
-              </title>
-              { font }
-              { css }
-            </head>
-            <body>
-              <div id="react-mount" dangerouslySetInnerHTML={ {    __html: this.props.body} } />
-              <script src={ prefixLink(`/bundle.js?t=${BUILD_TIME}`) } />
-            </body>
+                <head>
+                    <meta charSet="utf-8" />
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=5.0" />
+                    <title>
+                        {title}
+                    </title>
+                    <link rel="manifest" href="/manifest.json" />
+                    {font}
+                    {css}
+                </head>
+                <body>
+                    <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
+                    <script src={prefixLink('/register-worker.js')} />
+                    <script src={prefixLink(`/bundle.js?t=${BUILD_TIME}`)} />
+                </body>
+                <script>/* global document, window, require */
+                </script>
             </html>
         )
     },
