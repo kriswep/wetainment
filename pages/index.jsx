78a84dd15c5b72d850e6478b6628b59a2aa9be5b
Christoph Weber <kriswep@wetainment.com>
2016-12-07 21:42:07 +0100

    add key for react

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,53 +10,79 @@ import SitePost from '../components/SitePost'
 import SiteSidebar from '../components/SiteSidebar'
 
 class SiteIndex extends React.Component {
-    render() {
-        const pageLinks = []
-        // Sort pages.
-        const sortedPages = sortBy(this.props.route.pages, (page) => access(page, 'data.date')
-        ).reverse()
-        sortedPages.forEach((page) => {
-            if (access(page, 'file.ext') === 'md' && access(page, 'data.layout') === 'post') {
-                const title = access(page, 'data.title') || page.path
-                const description = access(page, 'data.description')
-                const datePublished = access(page, 'data.date')
-                const category = access(page, 'data.category')
+  render () {
+    // const pageLinks = []
+    // Sort pages.
+    const sortedPages = sortBy(this.props.route.pages, (page) => access(page, 'data.date')
+    ).reverse()
+    const pageLinks = sortedPages.filter((page) => {
+      if (access(page, 'file.ext') === 'md' && access(page, 'data.layout') === 'post') {
+        return true
+      } else {
+        return false
+      }
+    }).map((page, idx) => {
+      const title = access(page, 'data.title') || page.path
+      const description = access(page, 'data.description')
+      const datePublished = access(page, 'data.date')
+      const category = access(page, 'data.category')
 
-                pageLinks.push(
-                    <div className='blog-post'>
-                      <time dateTime={ moment(datePublished).format('MMMM D, YYYY') }>
-                        { moment(datePublished).format('MMMM YYYY') }
-                      </time>
-                      <span style={ {    padding: '5px'} }></span>
-                      <span className='blog-category'>{ category }</span>
-                      <h2><Link style={ {    borderBottom: 'none',} } to={ prefixLink(page.path) } > { title } </Link></h2>
-                      <p dangerouslySetInnerHTML={ {    __html: description} } />
-                      <Link className='readmore' to={ prefixLink(page.path) }> Read
-                      </Link>
-                    </div>
-                )
-            }
-        })
+      return (
+        <div className='blog-post' key={idx}>
+          <time dateTime={ moment(datePublished).format('MMMM D, YYYY') }>
+            { moment(datePublished).format('MMMM YYYY') }
+          </time>
+          <span style={ {    padding: '5px'} } />
+          <span className='blog-category'>{ category }</span>
+          <h2><Link style={ {    borderBottom: 'none',} } to={ prefixLink(page.path) } > { title } </Link></h2>
+          <p dangerouslySetInnerHTML={ {    __html: description} } />
+          <Link className='readmore' to={ prefixLink(page.path) }> Read
+          </Link>
+        </div>
+      )
+    })
+    // sortedPages.forEach((page) => {
+    //     if (access(page, 'file.ext') === 'md' && access(page, 'data.layout') === 'post') {
+    //         const title = access(page, 'data.title') || page.path
+    //         const description = access(page, 'data.description')
+    //         const datePublished = access(page, 'data.date')
+    //         const category = access(page, 'data.category')
 
-        return (
-            <DocumentTitle title={ config.siteTitle }>
-              <div>
-                <SiteSidebar {...this.props}/>
-                <div className='content'>
-                  <div className='main'>
-                    <div className='main-inner'>
-                      { pageLinks }
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </DocumentTitle>
-        )
-    }
+    //         pageLinks.push(
+    //             <div className='blog-post'>
+    //               <time dateTime={ moment(datePublished).format('MMMM D, YYYY') }>
+    //                 { moment(datePublished).format('MMMM YYYY') }
+    //               </time>
+    //               <span style={ {    padding: '5px'} }></span>
+    //               <span className='blog-category'>{ category }</span>
+    //               <h2><Link style={ {    borderBottom: 'none',} } to={ prefixLink(page.path) } > { title } </Link></h2>
+    //               <p dangerouslySetInnerHTML={ {    __html: description} } />
+    //               <Link className='readmore' to={ prefixLink(page.path) }> Read
+    //               </Link>
+    //             </div>
+    //         )
+    //     }
+    // })
+
+  return (
+    <DocumentTitle title={ config.siteTitle }>
+      <div>
+        <SiteSidebar {...this.props}/>
+        <div className='content'>
+          <div className='main'>
+            <div className='main-inner'>
+              { pageLinks }
+            </div>
+          </div>
+        </div>
+      </div>
+    </DocumentTitle>
+  )
+}
 }
 
 SiteIndex.propTypes = {
-    route: React.PropTypes.object,
+  route: React.PropTypes.object,
 }
 
 export default SiteIndex
