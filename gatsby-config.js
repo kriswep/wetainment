91cf86f03532026e01f391cf77a27e8c789db958
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-08-10 21:53:17 +0200

    setup first md blog site

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,25 @@ module.exports = {
     title: 'wetainment',
     author: 'Christoph Benjamin Weber',
   },
-  plugins: ['gatsby-plugin-catch-links', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-images',
+        ],
+      },
+    },
+  ],
 };
