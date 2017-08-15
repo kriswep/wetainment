4975531c9e0d6716ff6ae909fa4683ddea1af4fe
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-08-15 21:32:27 +0200

    add prism theme

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 import 'normalize.css';
+import 'prismjs/themes/prism-okaidia.css';
 
 import './index.css';
 
