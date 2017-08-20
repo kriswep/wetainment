cc2b1eab7435a049b0146a112b13bba851778c3f
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-08-20 13:50:52 +0200

    add social links to sidebar

--- src/layouts/sidebar.js
+++ src/layouts/sidebar.js
@@ -14,15 +14,11 @@ const ProfileImage = styled.img`
 
 const H1 = styled.h1`font-size: 1.2rem;`;
 
-// const NavLink = styled(StyledLink)`
-//   display: none;
-//   color: green;
-//   `;
 const NavLink = styled(StyledLink)`
   text-decoration: underline;
   color: #222;
   font-weight: bold;
-  display: inline;
+  display: inline-block;
   padding: 0.4rem 1rem 0.4rem 0;
   margin: 0.2rem 0.2rem 0.2rem 0;
   @media (min-width: 768px) {
@@ -30,6 +26,15 @@ const NavLink = styled(StyledLink)`
   padding: 0.4rem 0;
   margin: 0.2rem 0;
   }
+`;
+
+const SocialLink = styled.a`
+  display: inline-block;
+  padding: 0.4rem 1rem;
+  margin: 0.2rem 0.2rem 0.2rem 0;
+  text-decoration: none;
+  background-color: #eee;
+  color: black;
 `;
 
 // const Sidebar = ({ data }) =>
@@ -58,6 +63,12 @@ const Sidebar = () =>
       <NavLink to="/">Articles</NavLink>
       <NavLink to="/about">About me</NavLink>
       <NavLink to="/contact">Contact me</NavLink>
+    </nav>
+
+    <nav>
+      <SocialLink href="https://twitter.com/kriswep">T</SocialLink>
+      <SocialLink href="https://github.com/kriswep">G</SocialLink>
+      <SocialLink href="mailto:kriswep@wetainment.com">M</SocialLink>
     </nav>
   </aside>);
 
