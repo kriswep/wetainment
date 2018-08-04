98ec01ce91922bccdeceedeede5979e1b5e84a8d
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-08-04 21:15:02 +0200

    jsify contact page

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const ContactPage = () => (
+  <Layout>
+    <h1>About me</h1>
+    <p>
+      My personal portfolio page. I'm a software developer living kind of in the center of Germany.
+    </p>
+    <p>
+      Working for a great medium sized company doing development and consulting in SAP environments.
+    </p>
+    <p>Personally interested in all thinks JS, this is what I'm talking about here mostly.</p>
+  </Layout>
+);
+
+export default ContactPage;
