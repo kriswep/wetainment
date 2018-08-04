ef76c2c45d792e0b62a55b3fb5e4905b7b15b9a8
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-08-04 20:58:10 +0200

    fix plain js pages' layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const NotFoundPage = () =>
-  (<div>
+import Layout from '../components/layout';
+
+const NotFoundPage = () => (
+  <Layout>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>);
+  </Layout>
+);
 
 export default NotFoundPage;
